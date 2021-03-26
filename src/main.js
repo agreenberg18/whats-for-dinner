f24bc2ca0c2686730df76b80af37fe2e14e97baf
Andrew Greenberg <andrewgreenberg@Andrews-MacBook-Air.local>
2021-03-25 22:23:29 -0400

    Tiny emitter residual in main.js deleted

--- src/main.js
+++ src/main.js
@@ -4,12 +4,6 @@ import router from './router'
 import store from './store'
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
-import Emitter from 'tiny-emitter'
-//import mitt from 'mitt';
-var emitter = new Emitter()
 
-// const emitter = mitt();
-// const app = createApp(App);
-// app.config.globalProperties.emitter = emitter;
 
 createApp(App).use(store).use(router).mount('#app')
